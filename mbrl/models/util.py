164eed8eab45c2084a443a2523f8818ea7d917cb
shenao-zhang <82402331+shenao-zhang@users.noreply.github.com>
2022-07-07 23:31:39 +0800

    ensemble

--- mbrl/models/util.py
+++ mbrl/models/util.py
@@ -38,8 +38,6 @@ class EnsembleLinearLayer(nn.Module):
         self.num_members = num_members
         self.in_size = in_size
         self.out_size = out_size
-        self.linear_layer = nn.Linear(self.in_size, self.out_size)
-        """
         self.weight = nn.Parameter(
             torch.rand(self.num_members, self.in_size, self.out_size)
         )
@@ -48,7 +46,7 @@ class EnsembleLinearLayer(nn.Module):
             self.use_bias = True
         else:
             self.use_bias = False
-        """
+
         self.elite_models: List[int] = None
         self.use_only_elite = False
 
@@ -60,17 +58,11 @@ class EnsembleLinearLayer(nn.Module):
             else:
                 return xw
         else:
-            xw = self.linear_layer(x)
-            if xw.dim == 2:
-                xw = torch.unsqueeze(xw, 0)
-#            xw = x.matmul(self.weight)
-            return xw
-            """
+            xw = x.matmul(self.weight)
             if self.use_bias:
                 return xw + self.bias
             else:
                 return xw
-            """
 
     def extra_repr(self) -> str:
         return (
